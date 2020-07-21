84eb7db82744924ef97699b140d738dbc76078a1
olyawh <42892491+olyawh@users.noreply.github.com>
2020-07-21 12:28:18 +0300

    models.py added

--- my_project12_v1/accounts/models.py
+++ my_project12_v1/accounts/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, 
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+from django.db import models
+from django.urls import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, display_name=None, password=None):
+        if not email:
+            raise ValueError("You should enter an email address")
+        if not display_name:
+            display_name = username
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            display_name=display_name
+        )    
+        user.set_password(password)
+        user.save()
+        return user
+
+
+    def create_superuser(self, email, username, display_name, password):
+        user = self.create_user(
+            email,
+            username,
+            display_name,
+            password
+        )    
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+
+
+
+
