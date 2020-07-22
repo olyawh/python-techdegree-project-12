a1e86688ce48a6f6d5a5cab8e3c2aafce3088439
olyawh <42892491+olyawh@users.noreply.github.com>
2020-07-22 11:33:49 +0300

    User model accounts app added

--- my_project12_v1/accounts/models.py
+++ my_project12_v1/accounts/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import (
 )
 
 from django.db import models
-from django.urls import timezone
 
 
 class UserManager(BaseUserManager):
@@ -37,6 +36,31 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
+
+class User(AbstractBaseUser, PermissionsMixin):
+    '''A user model'''
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+    display_name = models.CharField(max_length=140)
+    bio = models.CharField(max_length=140, blank=True, default='')
+    avatar = models.ImageField(blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['display_name', 'username']
+
+    def __str__(self):
+        return '@{}'.format(self.username)
+
+    def get_short_name(self):
+        return self.display_name
+
+    def get_long_name(self):
+        return '{} (@{})'.format(self.display_name, self.username)        
 
 
 
--- my_project12_v1/my_project12_v1/settings.py
+++ my_project12_v1/my_project12_v1/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+AUTH_USER_MODEL = "users_management.UserManage" 
+AUTH_USER_MODEL = "accounts.User"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -121,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = ( os.path.join('static'), )
