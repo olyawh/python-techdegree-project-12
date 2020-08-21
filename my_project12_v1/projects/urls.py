a9e63fdb74f4469dbd1015edc74a8921eb607930
olyawh <42892491+olyawh@users.noreply.github.com>
2020-08-21 11:45:33 +0300

    List projects by user view added

--- my_project12_v1/projects/urls.py
+++ my_project12_v1/projects/urls.py
@@ -5,6 +5,7 @@ from .views import (
                     CreateProjectView,
                     UpdateProjectView,
                     ProjectDeleteView,
+                    UserProjectListView,
                     )
 from . import views
 
@@ -12,6 +13,7 @@ app_name = 'projects'
 
 urlpatterns = [
     path('/', ProjectListView.as_view(), name='list_projects'),
+    path('user/<str:username>', UserProjectListView.as_view(), name='user_list_projects'),
     path('/project/new/', CreateProjectView.as_view(), name='project_create'),
     path('/project/<pk>/update/', UpdateProjectView.as_view(), name='project_update'),
     path('/project/<pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
--- my_project12_v1/projects/views.py
+++ my_project12_v1/projects/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import generic
 from .models import Project
-
-
+from accounts.models import User
 
 
 class ProjectListView(generic.ListView):
@@ -12,6 +11,19 @@ class ProjectListView(generic.ListView):
     template_name = 'all_projects.html'
     context_object_name = 'projects'
     ordering = ['-date_posted']
+    paginate_by = 3
+
+
+class UserProjectListView(generic.ListView):
+    '''User project list view'''
+    model = Project
+    template_name = 'user_projects.html'
+    context_object_name = 'projects'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Project.objects.filter(author=user).order_by('-date_posted')
 
 
 class ProjectDetailView(generic.DetailView):
@@ -45,11 +57,9 @@ class UpdateProjectView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateV
              ]
     template_name = 'project_form.html'
 
-
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
 
     def test_func(self):
         project = self.get_object()
