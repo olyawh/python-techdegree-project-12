f0a846110c7d7044af069d4162a6e494b1264b23
olyawh <42892491+olyawh@users.noreply.github.com>
2020-09-08 15:04:56 +0300

    Urls tests added

---
+++ my_project12_v1/accounts/tests/test_urls.py
@@ -0,0 +1,56 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+
+from accounts.views import (
+                            home,
+                            LoginView,
+                            LogoutView,
+                            SignupView,
+                            profile,
+                            ChangeProfileView
+)
+
+
+class  TestUrls(SimpleTestCase):
+    '''Testing urls of accounts app'''
+
+    def test_blog_home_url(self):
+        '''Testing accounts:blog-home url'''
+        url = reverse('accounts:blog-home')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, home)
+
+
+    def test_login_url(self):
+        '''Testing accounts:login url'''
+        url = reverse('accounts:login')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, LoginView)    
+
+
+    def test_logout_url(self):
+        '''Testing accounts:logout url'''
+        url = reverse('accounts:logout')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, LogoutView)   
+
+
+    def test_signup_url(self):
+        '''Testing accounts:signup url'''
+        url = reverse('accounts:signup')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, SignupView)      
+
+
+    def test_profile_url(self):
+        '''Testing accounts:profile url'''
+        url = reverse('accounts:profile')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, profile)        
+
+
+    def test_profile_update_url(self):
+        '''Testing accounts:profile_update url'''
+        url = reverse('accounts:profile_update', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ChangeProfileView)         
---
+++ my_project12_v1/projects/tests/test_urls.py
@@ -0,0 +1,120 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from projects.views import (
+                            ProjectListView, 
+                            RecomProjectListView,
+                            UserProjectListView,
+                            CreateProjectView,
+                            UpdateProjectView,
+                            ProjectDeleteView,
+                            ProjectDetailView,
+                            search_projects,
+                            ApplicationListView,
+                            CreateApplicationView,
+                            UserApplicationListView,
+                            UserProjectsApplicationsListView,
+                            UpdateApplicationView,
+                            view_notifications
+                            )
+
+
+
+class  TestUrls(SimpleTestCase):
+    '''Testing urls of projects app'''
+
+    def test_list_projects_url(self):
+        '''Testing projects:list_projects url'''
+        url = reverse('projects:list_projects')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ProjectListView)
+
+
+    def test_recom_list_projects_url(self):
+        '''Testing projects:recom_list_projects url'''
+        url = reverse('projects:recom_list_projects')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, RecomProjectListView)
+
+
+    def test_user_list_projects_url(self):
+        '''Testing projects:user_list_projects url'''
+        url = reverse('projects:user_list_projects', args=['testusername'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, UserProjectListView)
+
+
+    def test_project_create_url(self):
+        '''Testing projects:project_create url'''
+        url = reverse('projects:project_create')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, CreateProjectView)    
+
+
+    def test_project_update_url(self):
+        '''Testing projects:project_update url'''
+        url = reverse('projects:project_update', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, UpdateProjectView) 
+
+
+    def test_project_delete_url(self):
+        '''Testing projects:project_delete url'''
+        url = reverse('projects:project_delete', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ProjectDeleteView)    
+
+
+    def test_project_detail_url(self):
+        '''Testing projects:project_detail url'''
+        url = reverse('projects:project_detail', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ProjectDetailView) 
+
+
+    def test_project_search_url(self):
+        '''Testing projects:project_search url'''
+        url = reverse('projects:project_search')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, search_projects)     
+
+
+    def test_list_applications_url(self):
+        '''Testing projects:list_applications url'''
+        url = reverse('projects:list_applications')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ApplicationListView)   
+
+
+    def test_application_create_url(self):
+        '''Testing projects:application_create url'''
+        url = reverse('projects:application_create', args=['test-slug', '1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, CreateApplicationView)  
+
+
+    def test_user_list_applications_url(self):
+        '''Testing projects:user_list_applications url'''
+        url = reverse('projects:user_list_applications', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, UserApplicationListView)      
+
+
+    def test_applications_url(self):
+        '''Testing projects:applications url'''
+        url = reverse('projects:applications', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, UserProjectsApplicationsListView)      
+
+
+    def test_application_update_url(self):
+        '''Testing projects:application_update url'''
+        url = reverse('projects:application_update', args=['1'])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, UpdateApplicationView)    
+
+
+    def test_notifications_url(self):
+        '''Testing projects:notifications url'''
+        url = reverse('projects:notifications')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, view_notifications)     
