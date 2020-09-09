ea0a43cb0e777f90e6b774c1cb0c84bfcf79b2c3
olyawh <42892491+olyawh@users.noreply.github.com>
2020-09-09 14:50:39 +0300

    view tests added

---
+++ my_project12_v1/accounts/tests/test_views.py
@@ -0,0 +1,51 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from accounts.models import (
+                            UserManager,
+                            User,
+                            Skill,
+                            Profile
+                            )
+import json
+from django.contrib.auth import get_user_model
+
+
+class TestViews(TestCase):
+    '''Testing views/accounts'''
+
+
+    def setUp(self):
+        '''Set up function'''
+        self.client = Client()
+        self.signup_url = reverse('accounts:signup')
+        self.profile_url = reverse('accounts:profile')
+        self.user = get_user_model().objects.create_user(
+            username = 'John',
+            email = 'test@mail.com',
+            password = 'justtestpass'
+        )
+
+
+    def test_signup_view_POST(self):
+        '''Testing sign up view'''
+
+        resp = self.client.post(self.signup_url, {
+            'username': 'jimi',
+            'email': 'jim@mail.com',
+            'password1': 'justpasstest',
+            'password2': 'justpasstest'
+        })
+
+        self.assertEquals(resp.status_code, 302)    
+
+
+    def test_profile_view_POST(self):
+        '''Testing profile view'''
+
+        resp = self.client.post(self.profile_url, {
+            'user': self.user,
+            'bio': 'Hi i am Dora the explorer'
+        })
+
+        self.assertEquals(resp.status_code, 302)      
+
---
+++ my_project12_v1/projects/tests/test_views.py
@@ -0,0 +1,84 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+from projects.models import (
+                            Project,
+                            Position,
+                            Application
+                            )
+import json
+from django.contrib.auth import get_user_model
+
+
+class TestViews(TestCase):
+    '''Testing views/projects'''
+
+
+    def setUp(self):
+        '''Set up function'''
+        self.client = Client()
+        self.user = get_user_model().objects.create_user(
+            username = 'John',
+            email = 'test@mail.com',
+            password = 'justtestpass'
+        )
+        self.list_projects_url = reverse('projects:list_projects')
+        self.list_applications_url = reverse('projects:list_applications')
+        self.project_detail_url = reverse('projects:project_detail', args=['1'])
+
+        self.project1 = Project.objects.create(
+            title = 'project1',
+            content = 'An app for kids',
+            author = self.user
+        )
+        self.project_create_url = reverse('projects:project_create')
+        self.application_create_url = reverse('projects:application_create', args=['some-slug', '2'])
+
+
+
+   # def test_project_list_view_GET(self):
+    #    '''Testing GET method project list view'''
+     #   resp = self.client.get(self.list_projects_url)
+
+      #  self.assertEquals(resp.status_code, 200)
+       # self.assertTemplateUsed(resp, 'all_projects.html', 'layout.html')
+
+
+    def test_application_list_view_GET(self):
+        '''Testing GET method application list view'''
+        resp = self.client.get(self.list_applications_url)
+
+        self.assertEquals(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'all_applications.html', 'layout.html')
+
+
+    def test_project_detail_view_GET(self):
+        '''Testing GET method project detail view'''
+        resp = self.client.get(self.project_detail_url)
+
+        self.assertEquals(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'project.html', 'layout.html')
+
+
+    def test_create_project_view_POST(self):
+        '''Testing POST method create project view'''
+
+        resp = self.client.post(self.project_create_url, {
+            'title': 'project2',
+            'content': 'justtests',
+            'author': self.user
+        })
+
+        self.assertEquals(resp.status_code, 302)
+
+
+    def test_create_application_view_POST(self):
+        '''Testing POST method create application view'''
+
+
+        resp = self.client.post(self.application_create_url, {
+            'status': 'Accepted',
+        })
+
+        self.assertEquals(resp.status_code, 302)    
+
+
